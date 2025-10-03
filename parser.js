d1dcca03522158050791115c2a4cbba5d32c5c14
Aaqib <drop.mohamedaaqib@gmail.com>
2025-10-03 09:27:58 +0530

    restructured parser

--- parser.js
+++ parser.js
@@ -1,28 +1,94 @@
-let string = "(* 2 3)";
-function parseList() {
-  if (!string.startsWith("("))
-    return null;
+// const input = "2"
+// const input = "(+ 2)"
+// const input = "(+ 2 4)"
+// const input = "(+ ( + 1) 4)"
+// const input = "(+ ( + 1)(- 4))"
+const input = "(+ ( + 1)(- 4))"
+// const input = "43abc"
 
-  let i = 1;
-  function skipWhitespace() {
-    if (i === "") {
-      i++;
-    }
-  }
-  skipWhitespace()
-  const result = []
-  if (string[i] === ")") {
-    return result;
-  }
 
-  if (string[i] === ("+" || "/" || "*")) {
-    result.push(string[i])
-  }
-  skipWhitespace()
-
-  if (string[i].isNumber) {
-    result.push(string[i])
-  }
-
+function removeWhitespace(rest) {
+  return rest.replace(/^\s+/, "")
 }
-console.log(parseList(string));
+
+
+function parsePrimitive(rest) {
+  rest = removeWhitespace(rest)
+
+  let match = rest.match(/^\d/)
+  if (match) {
+    let num = Number(match[0]);
+    let next = rest.slice(match[0].length)
+    return [num, next]
+  }
+
+  return [null, rest]
+}
+
+function parseCompound(rest) {
+  rest = removeWhitespace(rest)
+
+  if (!rest.startsWith("(")) {
+    return [null, rest]
+  }
+
+  rest = rest.slice(1)
+  rest = removeWhitespace(rest)
+
+  let operator = rest[0]
+
+  if (!['+', '-', '/', '*'].includes(operator)) {
+    return [null, rest]
+  }
+
+  rest = rest.slice(1) // consume operator
+  rest = removeWhitespace(rest)
+
+  let expr = [operator]
+
+  while (rest.length > 0 && rest[0] !== ")") {
+    let [operand, next] = parseExpression(rest)
+
+    if (operand == null) {
+      return [null, rest]
+    }
+
+    expr.push(operand)
+    rest = removeWhitespace(next)
+  }
+
+  if (rest.startsWith(")")) {
+    rest = rest.slice(1) // consume ")"
+    return [expr, rest]
+  }
+
+  return [null, rest]
+}
+
+
+function parseExpression(rest) {
+  rest = removeWhitespace(rest)
+
+  if (rest.length === 0) {
+    return [null, rest]
+  }
+
+  if (rest[0] === "(") {
+    return parseCompound(rest)
+  } else if (/[0-9]/.test(rest[0])) {
+    return parsePrimitive(rest)
+  }
+
+  return [null, rest]
+}
+
+function parse(rest) {
+  let [node, next] = parseExpression(rest)
+
+  if (node !== null && next.length === 0) {
+    return node
+  }
+  return null
+}
+
+console.log(parse(input))
