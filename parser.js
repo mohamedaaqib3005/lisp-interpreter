f93f02db6f8abdecebe3e659ad16d3622e531384
Aaqib <drop.mohamedaaqib@gmail.com>
2025-10-06 18:12:07 +0530

    parseNumber,parseOperator,parseOperand

--- parser.js
+++ parser.js
@@ -3,26 +3,54 @@
 // const input = "(+ 2 4)"
 // const input = "(+ ( + 1) 4)"
 // const input = "(+ ( + 1)(- 4))"
-const input = "(+ ( + 1)(- 4))"
+// const input = "(+ ( + 1)(- 4))"
 // const input = "43abc"
+const input = "( * 15 3)  "
 
+const OPERATORS = ['+', '-', '*', '/']
 
 function removeWhitespace(rest) {
   return rest.replace(/^\s+/, "")
 }
 
+function parseNumber(rest) {
 
-function parsePrimitive(rest) {
-  rest = removeWhitespace(rest)
-
-  let match = rest.match(/^\d/)
+  let match = rest.match(/^\d+/)
   if (match) {
     let num = Number(match[0]);
     let next = rest.slice(match[0].length)
     return [num, next]
   }
-
   return [null, rest]
+
+}
+
+function parseOperator(rest) {
+  if (OPERATORS.includes(rest[0])) {
+    let operator = rest[0]
+    let next = rest.slice(1)
+    return [operator, next]
+  }
+  return [null, rest]
+}
+
+function parseOperand(rest) {
+  rest = removeWhitespace(rest)
+  const [node, next] = parseExpression(rest);
+  if (node !== null) {
+    return [node, next]
+  }
+  return [null, rest]
+}
+
+function parsePrimitive(rest) {
+  rest = removeWhitespace(rest)
+
+  let [num, next] = parseNumber(rest)
+  if (num !== null) {
+    return [num, next]
+  }
+  return [null, next]
 }
 
 function parseCompound(rest) {
@@ -35,35 +63,32 @@ function parseCompound(rest) {
   rest = rest.slice(1)
   rest = removeWhitespace(rest)
 
-  let operator = rest[0]
+  let [operator, next] = parseOperator(rest)
 
-  if (!['+', '-', '/', '*'].includes(operator)) {
+  if (operator == null) {
     return [null, rest]
   }
 
-  rest = rest.slice(1) // consume operator
-  rest = removeWhitespace(rest)
+  rest = removeWhitespace(next)
 
-  let expr = [operator]
+  let operands = []
 
-  while (rest.length > 0 && rest[0] !== ")") {
-    let [operand, next] = parseExpression(rest)
-
+  while (rest.length > 0) {
+    if (rest.startsWith(")")) {
+      rest = rest.slice(1)
+      return [[operator, ...operands], rest]
+    }
+    let [operand, nextOperand] = parseOperand(rest)
     if (operand == null) {
       return [null, rest]
     }
-
-    expr.push(operand)
-    rest = removeWhitespace(next)
+    operands.push(operand)
+    rest = nextOperand
   }
-
-  if (rest.startsWith(")")) {
-    rest = rest.slice(1) // consume ")"
-    return [expr, rest]
-  }
-
   return [null, rest]
 }
+
+
 
 
 function parseExpression(rest) {
@@ -72,19 +97,19 @@ function parseExpression(rest) {
   if (rest.length === 0) {
     return [null, rest]
   }
+  let result = parseCompound(rest);
+  if (result[0] !== null) {
+    return result;
 
-  if (rest[0] === "(") {
-    return parseCompound(rest)
-  } else if (/[0-9]/.test(rest[0])) {
-    return parsePrimitive(rest)
   }
 
-  return [null, rest]
+  return parsePrimitive(rest);
+
 }
 
 function parse(rest) {
   let [node, next] = parseExpression(rest)
-
+  next = removeWhitespace(next)
   if (node !== null && next.length === 0) {
     return node
   }
@@ -92,3 +117,9 @@ function parse(rest) {
 }
 
 console.log(parse(input))
+
+
+// PARSE NUMBER INSIDE PARSEPRIMITIVE
+// break down with functions
+// operator && operand separator
+// define the operators outside
