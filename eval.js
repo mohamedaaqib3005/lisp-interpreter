4da2e2fabfe463524bd618883a6c58785de25dfd
Aaqib <drop.mohamedaaqib@gmail.com>
2025-10-10 10:42:09 +0530

    unary,null,empty

--- eval.js
+++ eval.js
@@ -8,15 +8,17 @@
 
 // const node = ["+", ["+", 1], ["-", 4]];
 
-// const node = null;
 
-const node = ["*", 15, 3];
+// const node = ["*", 15, 3];
+
+const node = [];
+
 
 const env = {
 
-  "+": (arr) => arr.reduce((a, b) => a + b),
+  "+": (arr) => arr.reduce((a, b) => a + b, 0),
 
-  "-": (arr) => arr.reduce((a, b) => a - b),
+  "-": (arr) => arr.length === 1 ? -arr[0] : arr.reduce((a, b) => a - b),
 
   "*": (arr) => arr.reduce((a, b) => a * b, 1,),
 
@@ -26,11 +28,21 @@ const env = {
 
 
 function Eval(node) {
+  if (node === null || node === undefined) {
+    return null;
+  }
+
+  if (Array.isArray(node) && node.length === 0) {
+    console.log("empty expression");
+    return null;
+  }
   const operator = node[0];
   const operands = node.slice(1);
   const values = operands.map(op => Array.isArray(op) ? Eval(op) : op);
   const fn = env[operator]
-  if (!fn) return null;
+  if (!fn) {
+    throw new Error(` Operator not present '${operator}'`);
+  };
   return fn(values)
 }
 
